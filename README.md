3617f7e4c3aa0878fdeb9930cd41152315c6df20
Fynn <firyan170@gmail.com>
2022-08-16 02:34:52 +0700

    ClashTermux

---
+++ README.md
@@ -0,0 +1,15 @@
+# ClashForTermux
+  ## Install
+   - ```pkg update && pkg upgrade -y```
+
+   - ```pkg install git -y```
+
+   - ```git clone https://github.com/fyn170/ClashTermux.git```
+
+   - ```cd ClashTermux```
+
+   - ```bash install.sh```
+
+   - ```cd $HOME/.config/clash```
+
+   - ```clash```
---
+++ install.sh
@@ -0,0 +1,73 @@
+#! /usr/bin/bash
+
+null="> /dev/null 2>&1"
+g="\033[1;32m"
+r="\033[1;31m"
+b="\033[1;34m"
+w="\033[0m"
+sleep 2
+echo -e $b"➤ "$w"Clash For Termux Non Root"
+echo ""
+echo -e $b"➤ "$w" installing dependencies ..."
+echo ""
+echo -e $b"➤ "$w" installing ... "$g"clash"$w
+pkg install clash
+echo ""
+echo -e $b"➤ "$w" installing ... "$g"nano"$w
+pkg install nano
+echo ""
+echo -e $b"➤ "$w" installing ... "$g"unzip"$w
+pkg install unzip
+echo ""
+
+mkdir -p $HOME/.config/clash
+echo ""
+mkdir -p $HOME/.config/clash/akun
+echo ""
+mkdir -p $HOME/.config/clash/core
+echo ""
+mkdir -p $HOME/.config/clash/rule
+echo ""
+mkdir -p $HOME/.config/clash/dashboard
+echo ""
+
+cd $HOME/ClashTermux
+sleep 2
+echo -e $b"➤ "$w" coppying akun "$g"akun"$w
+cp -r -f akun $HOME/.config/clash/
+echo ""
+sleep 2
+echo -e $b"➤ "$w" coppying config "$g"config"$w
+cp -r -f config.yaml $HOME/.config/clash/
+echo ""
+sleep 2
+echo -e $b"➤ "$w" coppying core "$g"core"$w
+cp -r -f core.zip $HOME/.config/clash/
+echo ""
+sleep 2
+echo -e $b"➤ "$w" coppying rule "$g"rule"$w
+cp -r -f core.zip $HOME/.config/clash/
+echo ""
+sleep 2
+echo -e $b"➤ "$w" coppying dashboard "$g"dashboard"$w
+cp -r -f dashboard.zip $HOME/.config/clash/
+echo ""
+
+sleep 2
+
+cd $HOME/.config/clash/
+echo -e $b"➤ "$w" extracting core "$g"core"$w
+unzip core.zip
+echo ""
+echo -e $b"➤ "$w" extracting dashboard "$g"dashboard"$w
+unzip dashboard.zip
+echo ""
+
+sleep 2
+rm -r -f core.zip
+rm -r -f dashboard.zip
+rm -r -f ClashTermux
+
+clear
+cd
+cd .config/clash
